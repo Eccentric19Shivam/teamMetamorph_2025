2bfd10aed6c14779b1a7c370511f62ac84842ee5
Eccentric19Shivam <singhshivamhp16@gmail.com>
2025-02-01 15:31:59 +0530

    more

--- assets/team.ts
+++ assets/team.ts
@@ -3,7 +3,7 @@ export const teamData =[
    "Name": "Nischey Karol",
    "Post": "Club Coordinator",
    "Roll no.": "22BMS020",
-   "png format pic": "https://drive.google.com/drive/folders/1cBlFj2NoNrk7O5mXWyN1W4XC6V1yvf2w",
+   "png format pic": "members/3rd_year/nischey3.jpg",
    "life motto": "BE THE LIGHT ",
    "linked in url": "https://www.linkedin.com/in/nischey-karol-308640266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/nischey_karol?igsh=MWYxYnQxY2F3Ymk5ZQ=="
@@ -12,16 +12,16 @@ export const teamData =[
    "Name": "Amolak Gurjar ",
    "Post": "Club Coordinator",
    "Roll no.": "22BMS004",
-   "png format pic": "https://drive.google.com/file/d/1RHaFnnIMGUOQYokDLrILMaWIaXPNQ88b/view?usp=drivesdk",
+   "png format pic": "members/3rd_year/amolakh3.png",
    "life motto": "Trim the excess, focus on the story, and keep the flow seamless.",
-   "linked in url": "",
-   "insta url": ""
+   "linked in url": "#home",
+   "insta url": "#home"
  },
  {
    "Name": "Arjun Dhania",
    "Post": "Executive",
    "Roll no.": "23BMS006",
-   "png format pic": "https://drive.google.com/file/d/1CwcY5j_I2wFrbLdHCcm8E7WpNg9WQEf8/view?usp=drivesdk",
+   "png format pic": "members/2nd_year/arjun2.png",
    "life motto": "Do not go gentle into that good night.",
    "linked in url": "https://www.linkedin.com/in/arjun-dhania-385209296?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/arjun___026?igsh=MWp5ZjJwZGowNmR0YQ=="
@@ -30,16 +30,16 @@ export const teamData =[
    "Name": "Aryan",
    "Post": "Executive",
    "Roll no.": "23BMS007",
-   "png format pic": "https://drive.google.com/file/d/1nTNjxDy9qnLpFa9uBGZgVGUO6zmf2Q7G/view?usp=drive_link",
-   "life motto": "\"He who conquers himself is the mightiest warrior\"",
-   "linked in url": "",
+   "png format pic": "members/2nd_year/aryan2.png",
+   "life motto": "He who conquers himself is the mightiest warrior",
+   "linked in url": "#home",
    "insta url": "https://www.instagram.com/ar.y.a.n_09?igsh=MW1vbTNxNDh2N2QyMA=="
  },
  {
    "Name": "Ayush Verma",
    "Post": "Executive",
    "Roll no.": "23BMS009",
-   "png format pic": "https://drive.google.com/file/d/11QehciyClw7dcazmrYfa-9soDpb4zhQ0/view?usp=drivesdk",
+   "png format pic": "members/2nd_year/ayush2.jpg",
    "life motto": "Fostering peace through sustainable wealth.",
    "linked in url": "https://www.linkedin.com/in/ayush-verma-a1a9392ba?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/amayush__/profilecard/?igsh=MTdvNW5tZ2RsdTF3bg=="
@@ -48,25 +48,25 @@ export const teamData =[
    "Name": "Devansh negi",
    "Post": "Executive",
    "Roll no.": "23BMS013",
-   "png format pic": "https://drive.google.com/file/d/1hP216j2aJv4VOX201sz2XdzDW0FmJ9Hb/view?usp=sharing",
+   "png format pic": "members/2nd_year/devansh2.png",
    "life motto": "To strive,to seek,to find and not to yield.",
    "linked in url": "https://www.linkedin.com/in/devansh-negi-4a2b822a9/",
-   "insta url": ""
+   "insta url": "#home"
  },
  {
    "Name": "Arshad Khan",
    "Post": "Executive",
    "Roll no.": "23BMS020",
-   "png format pic": "https://drive.google.com/file/d/1nLo2EGle-JP3mclzkYkr_ZViDw0l5G4d/view?usp=drivesdk",
+   "png format pic": "members/2nd_year/arshad2.png",
    "life motto": "Jaat Buddhi ",
-   "linked in url": "",
-   "insta url": ""
+   "linked in url": "#home",
+   "insta url": "#home"
  },
  {
    "Name": "Ruchika",
    "Post": "Executive",
    "Roll no.": "23BMS030",
-   "png format pic": "https://drive.google.com/file/d/1hARNrAMR8LXgYC523Mk0RyzKgzuhkKg0/view?usp=drivesdk",
+   "png format pic": "members/2nd_year/ruchika2.png",
    "life motto": "Being happy for what i have,while working on what i want",
    "linked in url": "https://www.linkedin.com/in/ruchika-k-34b1882b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/ruchi_804?igsh=MWw5b2cwZWsza3Fmaw=="
@@ -75,8 +75,8 @@ export const teamData =[
    "Name": "Sareha",
    "Post": "Executive",
    "Roll no.": "23BMS034",
-   "png format pic": "https://drive.google.com/file/d/1Go0gVJWjnl6aDeb3PB2oThRUhv_-DC_H/view?usp=drivesdk",
-   "life motto": "\"Forge ahead, no matter the pressure\"",
+   "png format pic": "members/2nd_year/sareha2.jpg",
+   "life motto": "Forge ahead, no matter the pressure",
    "linked in url": "https://www.linkedin.com/in/sareha-singh-b05a7628b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/sareha_singh?igsh=dW52MXdsaHlzdmJv"
  },
@@ -84,7 +84,7 @@ export const teamData =[
    "Name": "Sarika yadav",
    "Post": "Executive",
    "Roll no.": "23BMS035",
-   "png format pic": "https://drive.google.com/file/d/11AW734U_76Yc2XLoW2bB78wZKHgC3Z7N/view?usp=drivesdk",
+   "png format pic": "members/2nd_year/sarika2.jpg",
    "life motto": "Be grateful",
    "linked in url": "https://www.linkedin.com/in/sarika-yadav-6a6468323",
    "insta url": "https://www.instagram.com/sarika_yadav18?igsh=MWxhbXhhd2xkcHU5cA=="
@@ -93,7 +93,7 @@ export const teamData =[
    "Name": "Suffan Preet Kaur",
    "Post": "Executive",
    "Roll no.": "23BMS037",
-   "png format pic": "https://drive.google.com/file/d/11OdB2AnKPBi84XwumngmOeu2qdm8zWGx/view?usp=drivesdk",
+   "png format pic": "members/2nd_year/suffan2.jpg",
    "life motto": "This too shall pass ",
    "linked in url": "https://www.linkedin.com/in/suffanpreetkaur?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/_.suffan27?igsh=MXZ0N3dyMWpuNHF0OA=="
@@ -102,8 +102,8 @@ export const teamData =[
    "Name": "Tanishq Parashar",
    "Post": "Executive",
    "Roll no.": "23BMS039",
-   "png format pic": "https://drive.google.com/file/d/1mCWhSV-KRcydfAq0AkaAeKGSlMY5Ojw5/view?usp=drivesdk",
-   "life motto": "\"Learn, connect, and innovate to grow.\"",
+   "png format pic": "members/2nd_year/tanishq2.png",
+   "life motto": "Learn, connect, and innovate to grow.",
    "linked in url": "https://www.linkedin.com/in/tanishq-parashar-817252292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/tanishqparashar8119?igsh=MnZzazMwbDN3dWU1"
  },
@@ -111,26 +111,26 @@ export const teamData =[
    "Name": "Bukka Sumith Chandra Nag",
    "Post": "Executive",
    "Roll no.": "23BMS011",
-   "png format pic": "https://drive.google.com/file/d/1oQMRZ1oMEWA8IHLQJ1drba9CcrZNMLFK/view?usp=drivesdk",
+   "png format pic": "members/2nd_year/sumith2.png",
    "life motto": "Think exceptional and implement it!",
-   "linked in url": "",
-   "insta url": ""
+   "linked in url": "#home",
+   "insta url": "#home"
  },
  {
    "Name": "Sagar Baddhan",
    "Post": "Executive",
    "Roll no.": "23BMS031",
-   "png format pic": "https://drive.google.com/file/d/1D8kJe8oUH1pWq6d9utby1weST1liJtRh/view?usp=drive_link",
-   "life motto": "\"Don't bother to be better than others. Try to be better than yourself.\" ",
-   "linked in url": "",
+   "png format pic": "members/2nd_year/sagar2.png",
+   "life motto": "Don't bother to be better than others. Try to be better than yourself.",
+   "linked in url": "#home",
    "insta url": "https://www.instagram.com/sagar_baddhan?igsh=c3l1ZGRhbDdvbDZv"
  },
  {
    "Name": "Priyanshu Chaudhary",
    "Post": "Executive",
    "Roll no.": "23BME076",
-   "png format pic": "https://drive.google.com/file/d/1oAirqwPCjy9_kIqHzikk0PL3RatzmofR/view?usp=drivesdk",
-   "life motto": "\"Once you decided then never go back\"",
+   "png format pic": "members/2nd_year/priyanshu2.png",
+   "life motto": "Once you decided then never go back",
    "linked in url": "https://www.linkedin.com/in/priyanshu-chaudhary-009802278?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/priyanshupc.8755?igsh=eDM3OGx3OThueHpo"
  },
@@ -138,7 +138,7 @@ export const teamData =[
    "Name": "Harsh Dwivedi ",
    "Post": "Volunteer",
    "Roll no.": "24bms006",
-   "png format pic": "https://drive.google.com/file/d/11SvFIFwfyEjSPx7Wa62_CpARRcuPFHv_/view?usp=drivesdk",
+   "png format pic": "members/1st_year/harsh1.png",
    "life motto": "As long as I live ! There are infinite chances ",
    "linked in url": "https://www.linkedin.com/in/harsh-dwivedi-a9bb68266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/harsh_dwivedi11?igsh=MXZ0MTF2MXM2MGdndQ=="
@@ -147,7 +147,7 @@ export const teamData =[
    "Name": "Himanshu Meena ",
    "Post": "Volunteer",
    "Roll no.": "24BMS008",
-   "png format pic": "https://drive.google.com/file/d/11qlhYgerKA8krDweWtea8DmkPHw880aH/view?usp=drivesdk",
+   "png format pic": "members/1st_year/himanshu1.jpg",
    "life motto": "Dream Big, Act Bold",
    "linked in url": "https://www.linkedin.com/in/himanshu-meena-4b15b1323?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/i_himanshu_57?igsh=MTE2NWt5cXZ6MnM4bA=="
@@ -156,7 +156,7 @@ export const teamData =[
    "Name": "Molik Gopal Thakur ",
    "Post": "Volunteer",
    "Roll no.": "24BMS013",
-   "png format pic": "https://drive.google.com/file/d/18sDn8mLQP2osOY1M2d0LFgHUhDjnOe32/view?usp=drivesdk",
+   "png format pic": "members/1st_year/molik1.jpg",
    "life motto": "Dare to Try, Strive for Perfection",
    "linked in url": "https://www.linkedin.com/in/molik-thakur-250006292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/molikthakurr?igsh=MWF3NzE4NGxjdGFwMA=="
@@ -165,8 +165,8 @@ export const teamData =[
    "Name": "Prakriti",
    "Post": "Volunteer",
    "Roll no.": "24BMS016",
-   "png format pic": "https://drive.google.com/file/d/1ILFQeWCiQA8zUMEp_kf1K-6Y5bPd-abU/view?usp=drivesdk",
-   "life motto": "Pray,trust,wait.",
+   "png format pic": "members/1st_year/prakriti1.png",
+   "life motto": "Pray, trust, wait.",
    "linked in url": "www.linkedin.com/in/prakriti-billantu-481024349",
    "insta url": "https://www.instagram.com/prakriti.negi_?igsh=eHM4Y3N2M3NkemY3"
  },
@@ -174,7 +174,7 @@ export const teamData =[
    "Name": "Purva Uppal",
    "Post": "Volunteer",
    "Roll no.": "24BMS018",
-   "png format pic": "https://drive.google.com/file/d/1xhf6UjuZHEQlVGW4eD6pVt_uyOivM74I/view?usp=drivesdk",
+   "png format pic": "members/1st_year/purva1.png",
    "life motto": "Believe you can and you are halfway there!",
    "linked in url": "www.linkedin.com/in/purva-uppal-b8300a349",
    "insta url": "https://www.instagram.com/purva_uppal_36?igsh=YmYzZ3ltbXJ3aWFv"
@@ -183,7 +183,7 @@ export const teamData =[
    "Name": "Sanskar Bhangalia",
    "Post": "Volunteer",
    "Roll no.": "24BMS020",
-   "png format pic": "https://drive.google.com/file/d/1a5ihp7h9C_vNncJPDtez_WvW7yboNFza/view?usp=drivesdk",
+   "png format pic": "members/1st_year/sanskar1.jpg",
    "life motto": "Live as if you will die today",
    "linked in url": "Sanskar Bhangalia - National Institute of Technology Hamirpur-Alumni",
    "insta url": "https://www.instagram.com/sanskar_057?igsh=MTFsbzRoZjhxa3p1cA=="
@@ -192,26 +192,26 @@ export const teamData =[
    "Name": "Vinay Sharma",
    "Post": "Volunteer",
    "Roll no.": "24BMS024 ",
-   "png format pic": "https://drive.google.com/file/d/1OAazfXNNFBxl94UDYGINIPVG5soDwleT/view?usp=drivesdk",
+   "png format pic": "members/1st_year/vinay1.png",
    "life motto": "GO WITH THE FLOW",
    "linked in url": "https://linkedin.com/in/vinay-sharma-3236b0263",
-   "insta url": ""
+   "insta url": "#home"
  },
  {
    "Name": "Dilip kumar",
    "Post": "Volunteer",
    "Roll no.": "24BMS029",
-   "png format pic": "https://drive.google.com/file/d/17eOBsfTKQn8Q4OFgv69CHI84VtPoEak5/view?usp=drivesdk",
-   "life motto": "\"Only I can change my life. No one can do it for me\"",
-   "linked in url": "",
-   "insta url": ""
+   "png format pic": "members/1st_year/dilip1.png",
+   "life motto": "Only I can change my life. No one can do it for me.",
+   "linked in url": "#home",
+   "insta url": "#home"
  },
  {
    "Name": "Kishan Raj",
    "Post": "Volunteer",
    "Roll no.": "24BMS031",
-   "png format pic": "https://drive.google.com/file/d/1ACQaukTTrAGRE5SMoOSRuMHW6Y8jgETS/view?usp=drivesdk",
-   "life motto": "make it simple but significant",
+   "png format pic": "members/1st_year/kishan1.png",
+   "life motto": "Make it simple but significant.",
    "linked in url": "https://www.linkedin.com/in/kishan-raj-b3b5a9348/?trk=opento_sprofile_topcard#:~:text=www.linkedin.com/in/kishan%2Draj%2Db3b5a9348",
    "insta url": "https://www.instagram.com/kishan_yaduwanshi__/profilecard/?igsh=bWgybWFuOGJlaHpw"
  },
@@ -219,7 +219,7 @@ export const teamData =[
    "Name": "Kunal Saheb",
    "Post": "Volunteer",
    "Roll no.": "24BMS032",
-   "png format pic": "https://drive.google.com/file/d/10ocCaEj32zlUL59tzdbqlWxEvEHNpNTk/view?usp=drivesdk",
+   "png format pic": "members/1st_year/kunal1.png",
    "life motto": "Dream big work hard",
    "linked in url": "https://www.linkedin.com/in/kunal-saheb-b56b3b347",
    "insta url": "https://www.instagram.com/kunal_saheb_45?igsh=bWJsbnBieXZzZ2N4"
@@ -228,7 +228,7 @@ export const teamData =[
    "Name": "Shobha Kashyap",
    "Post": "Volunteer",
    "Roll no.": "24BMS035",
-   "png format pic": "https://drive.google.com/file/d/125w3SDfb3BUhw_eORbP2Ck_Cja3tCeuK/view?usp=drivesdk",
+   "png format pic": "members/1st_year/shobha1.png",
    "life motto": "Hustle hard",
    "linked in url": "https://www.linkedin.com/in/shobha-kashyap-49a908348",
    "insta url": "https://www.instagram.com/shobhaaa_07/profilecard/?igsh=MXVjdDRjbWZ1bzZsZQ=="
@@ -237,7 +237,7 @@ export const teamData =[
    "Name": "Umang Gautam",
    "Post": "Volunteer",
    "Roll no.": "24BMS037",
-   "png format pic": "https://drive.google.com/file/d/1_x97x_UXqG3_2yTsA_K54k0yuaGZ9QUN/view?usp=drivesdk",
+   "png format pic": "members/1st_year/umang1.png",
    "life motto": "सा विद्या या विमुक्तये ",
    "linked in url": "https://www.linkedin.com/in/umang-gautam-357623310?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/umang_.gautam?utm_source=qr&igsh=cmQ1OXJiazRycjgy"
@@ -246,7 +246,7 @@ export const teamData =[
    "Name": "Abha acharya",
    "Post": "Volunteer",
    "Roll no.": "24BMS001",
-   "png format pic": "https://drive.google.com/file/d/1ErCZEXQlVFhpKD5YTfAS8zpJJoXu0TgB/view?usp=drivesdk",
+   "png format pic": "members/1st_year/abha1.jpg",
    "life motto": "Pain is temporary, glory is permanent ",
    "linked in url": "https://www.linkedin.com/in/aabha-acharya-b70a96323?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/abhaacharyaa?igsh=MXJzZXJzZnNtOHE5ZA=="
@@ -255,16 +255,16 @@ export const teamData =[
    "Name": "Ajay Singh ",
    "Post": "Volunteer",
    "Roll no.": "24BMS002",
-   "png format pic": "https://drive.google.com/file/d/1I1X-dqkVidtiNWFYEQel26NS6mgd8w_2/view?usp=drivesdk",
+   "png format pic": "members/1st_year/ajay1.png",
    "life motto": "Consistency is the key.",
-   "linked in url": "",
+   "linked in url": "#home",
    "insta url": "https://www.instagram.com/ajaysingh_22_?igsh=YzljYTk1ODg3Zg=="
  },
  {
    "Name": "Arpit Kumar ",
    "Post": "Volunteer",
    "Roll no.": "24BMS003",
-   "png format pic": "https://drive.google.com/file/d/16N1LQMUvZJZls-Xd6u8Vv1k_AFTl_dJl/view?usp=drivesdk",
+   "png format pic": "members/1st_year/arpit1.jpg",
    "life motto": "Dream it ! Achieve it",
    "linked in url": "https://www.linkedin.com/in/ayush-gautam-260a7b308?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
    "insta url": "https://www.instagram.com/me_arpit.kumar?igsh=MXE2NXBuOTR1MDRv"
